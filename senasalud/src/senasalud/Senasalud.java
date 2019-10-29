be541eb5bc37a0d5eeff2e6760f20d309dc25cb4
SebastianCastro <castrosebastian452@gmail.com>
2019-10-29 15:52:36 -0500

    Mi primer commit

---
+++ senasalud/src/senasalud/Senasalud.java
@@ -0,0 +1,20 @@
+
+package senasalud;
+
+import java.util.Scanner;
+
+
+public class Senasalud {
+
+   
+    public static void main(String[] args) {
+      String nom;   
+      Scanner nombre =new Scanner (System.in);
+      System.out.println("Hola bienvenido, ingrese su nombre");
+      nom=nombre.next();
+      System.out.println("su nombre es:" +nom);
+      
+      
+    }
+    
+}
